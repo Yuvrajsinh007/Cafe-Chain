153279289a0d27b040a32450e195de0b642e9a3b
YUVRAJSINH JADEJA <mr.yuvrajsinhjadeja007@gmail.com>
2025-12-27 18:52:22 +0530

    Initial

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ connectDB();
 const allowedOrigins = [
     "http://localhost:5173", 
     "http://localhost:3000",
-    "https://cafechain-tau.vercel.app" // Your Vercel Frontend URL
+    "https://cafe-chain.vercel.app/" // Your Vercel Frontend URL
 ];
 
 // --- Core Middleware ---
